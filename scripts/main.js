f2d865c059a7d8fc5f9c01b840c6732b36bafbf8
BonnieRobloxRIP <vasko.tanchevv@gmail.com>
2026-03-11 08:26:15 +0200

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-import "./features/shop/shop.js";
-import "./features/emotes.js";
-import "./features/player_lifecycle.js";
-import "./features/gameplay.js";
-import "./features/items.js";
-import "./features/bridge_throw.js";
